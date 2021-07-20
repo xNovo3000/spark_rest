40440fab33eb069f89899a2e18cb8addaded20d0
xNovo3000 <fontana.ciro.2001@gmail.com>
2021-07-20 16:31:22 +0200

    First commit

---
+++ example/spark_http_example.dart
@@ -0,0 +1,6 @@
+import 'package:spark_http/spark_http.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/spark_http_test.dart
@@ -0,0 +1,16 @@
+import 'package:spark_http/spark_http.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
